7ac2e69d5ad651544f5641d1305f20d5edd4352f
Yael.Ben-Michael <yael.ben-michael@gmail.com>
2020-12-29 01:35:02 +0200

    made it a class

--- Currency_Roulette_Game.py
+++ Currency_Roulette_Game.py
@@ -1,30 +1,44 @@
 import os
 os.getcwd()
 from exchangeratesapi import Api
+from myAbstract import MyAbstract
 
-api = Api()
+
+class Currency_Roulette_Game(MyAbstract):
+
+    def __init__(self, diff):
+        self.diff = diff
+
+    def welcome(self):
+        print(" \n Welcome to Currency Roulette Game! \n")
+
+    def get_input_from_pc(self):
+        api = Api()
+        api.get_rates()
+        t = api.get_rate('USD', target='ILS')
+        range1 = (t-(5-self.diff))
+        range2 = (t+(5-self.diff))
+        return (range1, range2)
+
+    def get_input_from_user(self):
+        user_guess = input("Guess what the ILS rate is for 1 Dollar:")
+        return float(user_guess)
+
+    def compare(self):
+        myrange = self.get_input_from_pc()
+        low_lim = myrange[0]
+        high_lim = myrange[1]
+        if low_lim <= self.get_input_from_user() <= high_lim:
+            return True
+        else:
+            return False
+
+    def play(self):
+        self.welcome()
+        return(self.compare())
 
 
-def get_money_interval(diff):
-    api.get_rates()
-    t = api.get_rate('USD', target='ILS')
-    range1 = (t-(5-diff))
-    range2 = (t+(5-diff))
-    return (range1, range2)
 
-def get_guess_from_user(diff):
-    user_guess = input("Guess what the ILS rate is for 1 Dollar:")
-    return float(user_guess)
-
-def play(diff):
-    print(" \n Welcome to Currency Roulette Game! \n")
-    myrange = get_money_interval(diff)
-    low_lim = myrange[0]
-    high_lim = myrange[1]
-    if low_lim <= get_guess_from_user(diff) <= high_lim:
-        print("True")
-    else:
-        print("False")
 
 
 
